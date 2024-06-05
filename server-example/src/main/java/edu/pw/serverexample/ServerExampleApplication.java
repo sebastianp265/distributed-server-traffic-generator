f4343731a9a00a24a4ad606807f10b09d07b7e89
Sebastian Pawliński <pawlinskisebastian@gmail.com>
2024-06-06 00:43:51 +0200

    add skeleton for server example

---
+++ server-example/src/main/java/edu/pw/serverexample/ServerExampleApplication.java
@@ -0,0 +1,13 @@
+package edu.pw.serverexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerExampleApplication.class, args);
+    }
+
+}
---
+++ server-example/src/test/java/edu/pw/serverexample/ServerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pw.serverexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
